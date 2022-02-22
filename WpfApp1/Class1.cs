4fba9208df6ea3f3ad9fc2f090f4b58586e1d4f2
Jasmin Nezirovic <Jasmin Nezirovic@DESKTOP-ONFLNHQ>
2022-02-22 15:44:08 +0100

    Add project files.

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Calc
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Rezultat { get; set; }
+
+        public void Sabiranje()
+        {
+            Rezultat = X + Y;
+        }
+
+        public void Oduzimanje()
+        {
+            Rezultat = X - Y;
+        }
+
+        public void Mnozenje()
+        {
+            Rezultat = X * Y;
+        }
+
+        public void Deljenje()
+        {
+            Rezultat = X / Y;
+        }
+
+        public void Modul()
+        {
+            Rezultat = X % Y;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Calc();
+        }
+
+        private void Sabiranje(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Calc c)
+            {
+                c.Sabiranje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+        private void Oduzimanje(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Calc c)
+            {
+                c.Oduzimanje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+        private void Mnozenje(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Calc c)
+            {
+                c.Mnozenje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+        private void Deljenje(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Calc c)
+            {
+                c.Deljenje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+        private void Modul(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Calc c)
+            {
+                c.Modul();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+    }
+}
